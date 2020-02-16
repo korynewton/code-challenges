47cacfbd872bbaf872e83f86cd51b4135316d67a
Kory Newton <korybnewton@gmail.com>
2020-02-16 13:59:10 -0800

    leetcode symmetric-tree problem

---
+++ leetcode/SymmetricTree/solution.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def check_roots(root1, root2):
+            if not root1 and not root2:
+                return True
+            if not root1 or not root2:
+                return False
+            if root1.val != root2.val:
+                return False
+            
+            return check_roots(root1.right, root2.left) and check_roots(root1.left, root2.right)
+        
+        return check_roots(root,root)
