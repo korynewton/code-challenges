b2f2347f4903b9a67c35ba81c8f1bc008a60473e
Kory Newton <korybnewton@gmail.com>
2020-04-24 17:58:56 -0700

    leetcode Merge Two Linked Lists

---
+++ leetcode/MergeTwoLL/solution.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  const preHead = new ListNode(-1);
+
+  let prev = preHead;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      prev.next = l1;
+      l1 = l1.next;
+    } else {
+      prev.next = l2;
+      l2 = l2.next;
+    }
+    prev = prev.next;
+  }
+
+  prev.next = l2 ? l2 : l1;
+
+  return preHead.next;
+};
