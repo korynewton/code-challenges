496374d9c9f243aa3d4ef68ff15aae26a7974713
Kory Newton <korybnewton@gmail.com>
2020-05-16 13:16:08 -0700

    daily coding problem 119

---
+++ daily-coding-problem/191/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        # sort by second element in each interval (end point)
+        intervals.sort(key=lambda x: x[1])
+        count = 0
+        current_min = float('-inf')
+        for interval in intervals:
+            if interval[0] >= current_min:
+                current_min = interval[1]
+                count += 1
+        # return difference of # of intervals vs # of overlaps
+        return len(intervals) - count
