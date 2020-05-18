b17661fb72b83fd22f721d009710a28e2c919a81
kory <yrok@macmini.local>
2020-05-18 10:43:21 -0700

    Permutations In A String leetcode

---
+++ leetcode/PermutationsInString/solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def does_match(self, s1_arr: list, s2_arr: list) -> bool:
+        for i in range(26):
+            if s1_arr[i] != s2_arr[i]:
+                return False
+        return True
+        
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        #if s1 is longer than s2 it is impossible for s1 to be a substring of s2
+        if len(s1) > len(s2): return False
+        
+        #map letter frequencies in s1 to a list
+        s1_arr = [0]*26
+        for char in s1:
+            s1_arr[ord(char) - ord('a')] += 1
+        
+        #using a sliding window map letter frequencies of s2
+        #window length is the length of s1
+        for i in range(len(s2)-len(s1)+1):
+            s2_arr = [0]*26
+            for j in range(len(s1)):
+                s2_arr[ord(s2[i+j]) - ord('a')] += 1
+            #after calculating the frequencies, see if the char frequencies
+            #of s1 and the window of s2 match
+            if self.does_match(s1_arr, s2_arr):
+                return True
+            
+        return False
+            
+        
+
+        
